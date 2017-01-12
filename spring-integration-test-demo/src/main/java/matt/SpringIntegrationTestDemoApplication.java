ffffeb46ab14ee1ca7285d8777e8e77bfc51dfc8
Matt Masuda <matt.masuda@aa.com>
2017-01-12 14:55:04 -0700

    Minor changes to make this more like a real application

--- spring-integration-test-demo/src/main/java/matt/SpringIntegrationTestDemoApplication.java
+++ spring-integration-test-demo/src/main/java/matt/SpringIntegrationTestDemoApplication.java
@@ -2,8 +2,10 @@ package matt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:META-INF/spring/spring-integration-context.xml")
 public class SpringIntegrationTestDemoApplication {
 
 	public static void main(String[] args) {
